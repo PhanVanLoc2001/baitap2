122602bd2b7dde1fd21cb86bbaf7d2afa288f1eb
PhanVanLoc2001 <phanvanlocxt@gmail.com>
2021-09-25 20:32:13 +0700

    baitap2

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Restaure website!</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <img src="images/logo.png" alt="" srcset="">
+                <ul class="nav justify-content-end  ">
+                    <li class="nav-item">
+                        <a class="nav-link active danger text-danger" aria-current="page" href="#">Home</a>
+                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-danger" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-danger" href="#">Foods</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-danger" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div> 
+        </div> 
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 ">
+                <div class="search">    
+                     <input type="search"   placeholder="Search..." aria-label="Search">
+                     <button type="button" class="btn btn-danger">Search</button>
+                </div>
+            </div>
+        </div>
+    </div>           
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12" style="text-align:center;">
+                <h1 class="fs-2">Explore Foods</h1>
+                <figure class="figure">
+                    <img src="images/burger.jpg" style="margin:0px 10px;width: 300px;" class="rounded float-start" alt="...">
+                    <img src="images/pizza.jpg" style="margin:0px 10px;width: 300px; class="rounded float-end" alt="...">
+                    <img src="images/momo.jpg" style="margin:0px 10px;width: 300px; class="rounded mx-auto d-block" alt="...">
+                </figure>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12" style="background-color:#ececec;height:650px;float:left;">
+                <h1 class="fs-2" style="text-align:center">Foods Menu</h1>
+                    <div class="card" style="width: 25rem;margin:0px 10px;float:left;">
+                        <img src="images/menu-pizza.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">$2.3</h5>
+                            <p class="card-text">made with italia sauce, chiken and organice vegetables.</p>
+                            <a href="#" class="btn btn-primary;btn btn-danger">Order Now</a>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 25rem;margin:0px 10px;float:left;">
+                        <img src="images/menu-pizza.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">$2.3</h5>
+                            <p class="card-text">made with italia sauce, chiken and organice vegetables.</p>
+                            <a href="#" class="btn btn-primary;btn btn-danger">Order Now</a>
+                        </div>
+                    </div>  
+                    <div class="card" style="width: 25rem;margin:0px 10px;float:left;">
+                        <img src="images/menu-pizza.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">$2.3</h5>
+                            <p class="card-text">made with italia sauce, chiken and organice vegetables.</p>
+                            <a href="#" class="btn btn-primary;btn btn-danger">Order Now</a>
+                        </div>
+                    </div>
+                <h4 style="text-align:center;color:red;">See all foods</h4>    
+            </div>      
+        </div>
+    </div>
+    <h6 style="text-align:center">
+        <img src="images/logo-fb.png" alt="" srcset="">
+        <img src="images/logo-insta.png" alt="" srcset="">
+        <img src="images/logo-twit.png" alt="" srcset="">
+        <p style="text-align:center;">All rights Designed By <a href="#" style="color:red">Vijay Thapa</a></p>
+</h6>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  </body>
+</html>
